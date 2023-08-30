d0e5d8b4a01a8115847ff525d29312f81303d610
Jared Stromberg <jstromberg@astraspace.net>
2023-08-30 12:51:54 -0700

    updating tars request case statements

--- tars/src/main.c
+++ tars/src/main.c
@@ -10,13 +10,15 @@
 #include "main.h"
 
 #define DATE "DATE"
+#define UPTIME "UPTIME"
+#define STATE "STATE"
 
 void fatal (const char *function, int return_value) {
     fprintf(stderr, "%s: %s\n", function, nng_strerror(return_value));
     exit(1);
 }
 
-int app1_reqrep_nng_node(const char *url) {
+int app1_reqrep_nng_node(const char *url, int request_type) {
     nng_socket sock;
     int return_value;
     size_t buffer_size;
@@ -25,22 +27,62 @@ int app1_reqrep_nng_node(const char *url) {
     if ((return_value = nng_req0_open(&sock)) != 0) {
         fatal("nng_socket", return_value);
     }
-
     if ((return_value = nng_dial(sock, url, NULL, 0)) != 0) {
         fatal("nng_dial", return_value);
     }
 
-    printf("TARS: SENDING DATE REQUEST to APP1 %s\n", DATE);
-    if ((return_value = nng_send(sock, DATE, strlen(DATE) + 1, 0)) != 0) {
-        fatal("nng_send", return_value);
-    }
-    if ((return_value = nng_recv(sock, &buffer, &buffer_size, NNG_FLAG_ALLOC)) != 0) {
-        fatal("nng_recv", return_value);
-    }
+    switch (request_type)
+    {
+    case 0:
+        printf("TARS: SENDING DATE REQUEST to APP1 %s\n", DATE);
+        if ((return_value = nng_send(sock, DATE, strlen(DATE) + 1, 0)) != 0) {
+            fatal("nng_send", return_value);
+        }
+        if ((return_value = nng_recv(sock, &buffer, &buffer_size, NNG_FLAG_ALLOC)) != 0) {
+            fatal("nng_recv", return_value);
+        }
 
-    printf("TARS: RECEIVED DATE %s\n", buffer);
-    nng_free(buffer, buffer_size);
-    nng_close(sock);
+        printf("TARS: RECEIVED DATE %s\n", buffer);
+        nng_free(buffer, buffer_size);
+        nng_close(sock);
+        
+        break;
+    
+    case 1:
+        printf("TARS: SENDING UPTIME REQUEST to APP1 %s\n", UPTIME);
+        if ((return_value = nng_send(sock, UPTIME, strlen(UPTIME) + 1, 0)) != 0) {
+            fatal("nng_send", return_value);
+        }
+        if ((return_value = nng_recv(sock, &buffer, &buffer_size, NNG_FLAG_ALLOC)) != 0) {
+            fatal("nng_recv", return_value);
+        }
+
+        printf("TARS: RECEIVED UPTIME %s\n", buffer);
+        nng_free(buffer, buffer_size);
+        nng_close(sock);
+        
+        break;
+
+    case 2:
+        printf("TARS: SENDING CURRENT STATE REQUEST TO APP1 %s\n", STATE);
+        if ((return_value = nng_send(sock, STATE, strlen(STATE) + 1, 0)) != 0) {
+            fatal("nng_send", return_value);
+        }
+        if ((return_value = nng_recv(sock, &buffer, &buffer_size, NNG_FLAG_ALLOC)) != 0) {
+            fatal("nng_recv", return_value);
+        }
+
+        printf("TARS: RECEIVED APP1 STATE %s\n", buffer);
+        // TODO: UPDATE TARS STATE MACHINE HERE WITH APP1 CURRENT STATE
+        nng_free(buffer, buffer_size);
+        nng_close(sock);
+
+        break;
+
+    default:
+        printf("TARS: INVALID REQUEST TYPE");
+        break;
+    }
 
     return 0;
 }
@@ -55,7 +97,7 @@ int main (void) {
     sub2();
 
     // app1__reqrep_nng_node("ipc:///tmp/reqrep.ipc");
-    app1_reqrep_nng_node("tcp://127.0.0.1:4001");
+    app1_reqrep_nng_node("tcp://127.0.0.1:4001", 0);
     return 0;
 }
 
